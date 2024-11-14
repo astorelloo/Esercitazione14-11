42afa41072b03b1c50a70bbfcb97b0ebf7c8b26e
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-14 07:45:11 +0000

    fase 2 punto 5

--- ListaSpesa/main.py
+++ ListaSpesa/main.py
@@ -13,3 +13,22 @@ def eliminaElemento():
     risp = input("inserisci il nome dell'elemento che vuoi eliminare")
     risp.lower()
     lista.remove(risp)
+
+controllo = True
+while controllo == True:
+    print("1) aggiungi un elemento alla lista")
+    print("2) visualizza la lista")
+    print("3) elimina un elemento dalla lista")
+    x = input("inserisci il numero dell'azione che vuoi eserguire:")
+    if x == "1":
+        aggiunta()
+    elif x== "2":
+        stampaLista()
+    elif x=="3":
+        eliminaElemento()
+    else:
+        uscita= input("vuoi uscire? (y/n)")
+        uscita.lower()
+        if uscita == "y":
+            controllo = False
+        
