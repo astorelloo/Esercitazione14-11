f2e52c88d9e846f4f9149d1e15beb6a076e498d9
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-14 07:58:54 +0000

    fase 3 finita

--- ListaSpesa/main.py
+++ ListaSpesa/main.py
@@ -14,11 +14,24 @@ def eliminaElemento():
     risp.lower()
     lista.remove(risp)
 
+def conteggio():
+    cont =0 
+    for i in range(len(lista)):
+        cont += 1
+    print(f"la lista contiene {cont} elementi")
+
+def svuotaLista():
+    for i in range(len(lista)):
+        lista.remove(lista[i-1])
+
 controllo = True
 while controllo == True:
+    print("------------------------------------")
     print("1) aggiungi un elemento alla lista")
     print("2) visualizza la lista")
     print("3) elimina un elemento dalla lista")
+    print("4) numero di elementi contenuti nella lista")
+    print("5) svuota la lista")
     x = input("inserisci il numero dell'azione che vuoi eserguire:")
     if x == "1":
         aggiunta()
@@ -26,6 +39,10 @@ while controllo == True:
         stampaLista()
     elif x=="3":
         eliminaElemento()
+    elif x== "4":
+        conteggio()
+    elif x== "5":
+        svuotaLista()
     else:
         uscita= input("vuoi uscire? (y/n)")
         uscita.lower()
