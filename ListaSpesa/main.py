f74a8392348450ea427757d61fd955a93c075882
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-14 07:31:53 +0000

    fase 2 punto 4

--- ListaSpesa/main.py
+++ ListaSpesa/main.py
@@ -2,7 +2,14 @@ lista = []
 
 def aggiunta():
     item = input("inserisci un elemento alla lista:")
+    item.lower()
     lista.append(item)
-    
+
 def stampaLista():
     print(lista)
+
+def eliminaElemento():
+    print(lista)
+    risp = input("inserisci il nome dell'elemento che vuoi eliminare")
+    risp.lower()
+    lista.remove(risp)
